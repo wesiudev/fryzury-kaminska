ee3104718514bce4e2ef33d7353865e8396c46bf
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-23 23:37:01 +0200

    admin panel

--- app/admin/new-image/page.tsx
+++ app/admin/new-image/page.tsx
@@ -2,7 +2,7 @@ import { getImages } from "../../../firebase/index";
 
 import UploadImage from "./UploadImage";
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/images`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/images`);
   const data = await res.json();
 }
 export default async function Page() {
