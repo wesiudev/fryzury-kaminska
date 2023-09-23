cf60edc22df7dc0267ffd28041a0371caa259f3f
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-23 23:37:40 +0200

    admin panel

--- app/admin/new-image/page.tsx
+++ app/admin/new-image/page.tsx
@@ -4,6 +4,7 @@ import UploadImage from "./UploadImage";
 async function getData() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/images`);
   const data = await res.json();
+  return data;
 }
 export default async function Page() {
   const images = await getData();
